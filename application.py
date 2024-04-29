26d01f1a6bec0fca70942a87356d1e29b2de0a7e
Venkat Raman <venkatraman00004@gmail.com>
2024-04-29 15:08:21 +0530

    Form Changes

--- application.py
+++ application.py
@@ -11,14 +11,14 @@ app = application
 
 # Route for home page
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
-        return render_template('home.html')
+        return render_template('testing.html')
     else:
         data = CustomData(
             gender=request.form.get('gender'),
@@ -35,7 +35,7 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
 
-        return render_template('home.html', results=results[0])
+        return render_template('testing.html',  results=results[0])
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
